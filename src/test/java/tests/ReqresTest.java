a440cd39221099f56870f3185a5a1316973238b7
roman <re_starkov@betboom.org>
2023-03-10 14:57:04 +0300

    added test for api with groovy

--- src/test/java/tests/ReqresTest.java
+++ src/test/java/tests/ReqresTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 
+import io.restassured.response.Response;
 import jdk.jfr.Description;
 import models.createuser.CreateBodyRequestModel;
 import models.createuser.CreateResponseModel;
@@ -8,88 +9,46 @@ import models.getuser.GetResponseModel;
 import models.getlist.GetListResponseModel;
 import models.updateuser.UpdateBodyRequestModel;
 import models.updateuser.UpdateResponseModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static specs.BaseSpec.requestSpec;
 import static specs.BaseSpec.responseSpec;
 
 public class ReqresTest {
 
-    @Description("Создание пользователя")
-    @Test
-    public void createUserTest() {
-
-        String id = "339";
-        String job = "leader";
-        String name = "morpheus";
-
-        CreateBodyRequestModel bodyData = new CreateBodyRequestModel();
-        bodyData.setId(id);
-        bodyData.setJob(job);
-        bodyData.setName(name);
-
-        CreateResponseModel response = step("Отправка запроса на создание пользователя",
-                () -> given(requestSpec)
-                        .body(bodyData)
-                        .when()
-                        .post("/users")
-                        .then()
-                        .spec(responseSpec)
-                        .statusCode(201)
-                        .extract().as(CreateResponseModel.class));
-
-        step("Проверка значения id полученного в ответе", () ->
-                assertThat(response.getId()).isEqualTo(id));
-        step("Проверка значения поля name полученного в ответе", () ->
-                assertThat(response.getName()).isEqualTo(name));
-        step("Проверка значения поля job полученного в ответе", () ->
-                assertThat(response.getJob()).isEqualTo(job));
-    }
 
     @Description("Получение данных по пользователю")
     @Test
-    public void getUserDataTest() {
+    public void getUserDataUsingGroovyTest() {
 
-        GetResponseModel response = step("Отправка запроса на получение данных о пользователе",
-                () -> given(requestSpec)
-                        .when()
-                        .get("/users/2")
-                        .then()
-                        .spec(responseSpec)
-                        .statusCode(200)
-                        .extract().as(GetResponseModel.class));
-
-        step("Проверка значения поля id полученного в ответе", () ->
-                assertThat(response.getData()).hasFieldOrPropertyWithValue("id", 2));
-        step("Проверка значения поля first_name полученного в ответе", () ->
-                assertThat(response.getData()).hasFieldOrPropertyWithValue("first_name", "Janet"));
-        step("Проверка значения поля last_name полученного в ответе", () ->
-                assertThat(response.getData()).hasFieldOrPropertyWithValue("last_name", "Weaver"));
-        step("Проверка значения поля text полученного в ответе", () ->
-                assertThat(response.getSupport()).hasFieldOrPropertyWithValue
-                        ("text", "To keep ReqRes free, contributions towards " +
-                                "server costs are appreciated!"));
+        given(requestSpec)
+                .when()
+                .get("/users/2")
+                .then()
+                .spec(responseSpec)
+                .statusCode(200)
+                .body("data.email", is("janet.weaver@reqres.in"));
     }
 
     @Description("Получение данных по списку пользователей, проверка имени во втором объекте массива")
     @Test
-    public void getUserListDataTest() {
+    public void getUserListDataUsingGroovyTest() {
 
-        GetListResponseModel response = step("Отправка запроса на получение списка пользователей",
-                () -> given(requestSpec)
-                        .when()
-                        .get("/users?page=2")
-                        .then()
-                        .spec(responseSpec)
-                        .statusCode(200)
-                        .extract().as(GetListResponseModel.class));
+        given(requestSpec)
+                .when()
+                .get("/users?page=2")
+                .then()
+                .spec(responseSpec)
+                .statusCode(200)
+                .body("data.findAll{it.id == 8}.first_name.flatten()", hasItem("Lindsay"));
 
-        step("Проверка значения поля first_name во втором объекте массива data", () ->
-                assertThat(response.getData().get(1)).hasFieldOrPropertyWithValue("first_name", "Lindsay"));
     }
 
     @Description("Проверка статус кода, при поиске несуществующего пользователя")
@@ -107,7 +66,7 @@ public class ReqresTest {
 
     @Description("Обновление данных по пользователю")
     @Test
-    public void updateUserTest() {
+    public void updateUserUsingGroovyTest() {
 
         String job = "zion resident";
         String name = "morpheus";
@@ -116,20 +75,26 @@ public class ReqresTest {
         request.setJob(job);
         request.setName(name);
 
-        UpdateResponseModel response = step("Отправка запроса на обновление данных о пользователе",
-                () -> given(requestSpec)
+        given(requestSpec)
 
-                        .body(request)
-                        .when()
-                        .patch("/users/2")
-                        .then()
-                        .spec(responseSpec)
-                        .statusCode(200)
-                        .extract().as(UpdateResponseModel.class));
+                .body(request)
+                .when()
+                .patch("/users/2")
+                .then()
+                .spec(responseSpec)
+                .statusCode(200)
+                .body("name", is(name))
+                .body("job", is(job));
+    }
 
-        step("Проверка значения поля name в ответе", () ->
-                assertThat(response.getName()).isEqualTo(name));
-        step("Проверка значения поля Job в ответе", () ->
-                assertThat(response.getJob()).isEqualTo(job));
+    @Test
+    public void checkEmailUsingGroovy() {
+        given(requestSpec)
+                .when()
+                .get("/users")
+                .then()
+                .log().body()
+                .body("data.findAll{it.email =~/.*?@reqres.in/}.email.flatten()",
+                        hasItem("janet.weaver@reqres.in"));
     }
 }
